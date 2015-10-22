c33991983517df3e1000c0197cfec1b1a3dd773d
Joao Olveira <ei11114@fe.up.pt>
2015-10-22 17:28:37 +0100

    Inital commit

---
+++ FirstREST/Controllers/OportunidadesDeVendaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstREST.Lib_Primavera.Model;
+
+namespace FirstREST.Controllers
+{
+    public class OportunidadesDeVendaController : ApiController
+    {
+        // GET api/oportunidadesdevenda
+        public IEnumerable<Lib_Primavera.Model.OportunidadeDeVenda> Get()
+        {
+            return Lib_Primavera.PriIntegration.ListaOportunidadesDeVenda();
+        }
+
+        // GET api/oportunidadesdevenda/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/oportunidadesdevenda
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/oportunidadesdevenda/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/oportunidadesdevenda/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FirstREST/Lib_Primavera/Model/OportunidadeDeVenda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class OportunidadeDeVenda
+    {
+        public string id
+        {
+            get;
+            set;
+        }
+
+        public string descricao
+        {
+            get;
+            set;
+        }
+    }
+}
