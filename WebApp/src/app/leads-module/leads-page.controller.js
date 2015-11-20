148a240688f20a293278fe259295d7ca0331a942
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-11-20 01:44:00 +0000

    QoL changes

--- WebApp/src/app/leads-module/leads-page.controller.js
+++ WebApp/src/app/leads-module/leads-page.controller.js
@@ -2,8 +2,8 @@
     'use strict';
 
     angular
-        .module('leads-module')
-        .controller('LeadPageController', LeadPageController);
+    .module('leads-module')
+    .controller('LeadPageController', LeadPageController);
 
     /* @ngInject */
     function LeadPageController(uiGmapGoogleMapApi) {
@@ -12,38 +12,37 @@
             url:'http://localhost:49822/api/leads/816B85C7-98E3-11DC-A3E8-0020E024149C/tasks', 
             type:'get', 
             success: function (response) {
-            for (var i = 0; i < response.length; i++) {
-                $('#task-div').before('<md-divider ></md-divider><md-list-item class="md-1-line"><div class="md-list-item-text"><p>' + response[i]['Resumo'] + '</p></div></md-list-item>');
-            }
-            console.log('done');
+                for (var i = 0; i < response.length; i++) {
+                    $('#task-div').before('<md-divider ></md-divider><md-list-item class="md-1-line"><div class="md-list-item-text"><p>' + response[i]['Resumo'] + '</p></div></md-list-item>');
+                }
             }});
         var vm = this;
         uiGmapGoogleMapApi.then(function(maps) {
             vm.terrainMap = {
                 center: {
-                    latitude: 51.219053,
-                    longitude: 4.404418
+                  latitude: 41.177875, 
+                  longitude: -8.597895
+              },
+              zoom: 20,
+              marker: {
+                id:0,
+                coords: {
+                    latitude: 41.177875, 
+                    longitude: -8.597895
                 },
-                zoom: 10,
-                marker: {
-                    id:0,
-                    coords: {
-                        latitude: 51.219053,
-                        longitude: 4.404418
-                    },
-                    options: {
-                        icon: {
-                            anchor: new maps.Point(36,36),
-                            origin: new maps.Point(0,0),
-                            url: 'assets/images/maps/blue_marker.png'
-                        }
+                options: {
+                    icon: {
+                        anchor: new maps.Point(36,36),
+                        origin: new maps.Point(0,0),
+                        url: 'assets/images/maps/blue_marker.png'
                     }
-                },
-                options:{
-                    scrollwheel:false,
-                    mapTypeId:maps.MapTypeId.TERRAIN
                 }
-            };
-        });
+            },
+            options:{
+                scrollwheel:false,
+                mapTypeId:maps.MapTypeId.TERRAIN
+            }
+        };
+    });
     }
 })();
