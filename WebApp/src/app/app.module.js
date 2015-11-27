acf516d2b14fd494fc0d6e2ffaa1937af00ebca1
lycantropus <mechanical.maggot.0@gmail.com>
2015-11-27 01:19:16 +0000

    ta com bud a passar do search task para pagina da task.

--- WebApp/src/app/app.module.js
+++ WebApp/src/app/app.module.js
@@ -7,7 +7,8 @@
             'ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngMaterial',
             'ui.router', 'pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'angularMoment', 'textAngular', 'uiGmapgoogle-maps', 'hljs', 'md.data.table', angularDragula(angular), 'ngFileUpload',
             'dashboard-module',
-            'leads-module'
+            'leads-module',
+            'tasks-module'
         ])
         // create a constant for languages so they can be added to both triangular & translate
         .constant('APP_LANGUAGES', [{
