0aa5e880207b5e0be025d928b82955b48a04ecfb
João Bordalo <bordalo.joao94@gmail.com>
2015-12-16 16:06:25 +0000

    returning invoices

--- FirstREST/Lib_Primavera/Model/Fatura.cs
+++ FirstREST/Lib_Primavera/Model/Fatura.cs
@@ -37,6 +37,12 @@ namespace FirstREST.Lib_Primavera.Model
             set;
         }
 
+        public double TotalIEC
+        {
+            get;
+            set;
+        }
+
         public double TotalOutros
         {
             get;
