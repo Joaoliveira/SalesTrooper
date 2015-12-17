b4efee62fa95fc18743361dba3e9c359217f8927
lycantropus <mechanical.maggot.0@gmail.com>
2015-12-17 07:21:38 +0000

    fix merge

--- WebApp/src/app/salesmen-module/salesmen-page.controller.js
+++ WebApp/src/app/salesmen-module/salesmen-page.controller.js
@@ -10,6 +10,16 @@
         var vm = this;
         vm.clientes = [];
 
+    vm.columns = [{
+            title: '',
+            field: '',
+            sortable: false,
+            filter: 'tableImage'
+        },{
+            title: 'Client',
+            field: 'entidade',
+            sortable: true
+        }]; 
         var promise = $http.get('http://localhost:49822/api/salesmen/' + 1/*$stateParams.salesmanID*/);
 
         promise.then(function requestDone (response) {
@@ -111,6 +121,8 @@
 
            }
 
+        
+
            function chartPieData() {
             vm.data = [];
 
@@ -126,11 +138,12 @@
         // Simulate async data update
         $interval(chartPieData, 5000);
 
-          });
+        
+        });
 
 
         // LINEAR CHART
-           vm.chartData = {
+        vm.chartData = {
             type: 'Line',
             data: {
                 'cols': [
@@ -193,93 +206,15 @@
             }
         };
 
- 
 
-
-        promise = $http.get('http://localhost:49822/api/salesmen/' + '1' + '/leads');
-
-        promise.then(function requestDone (response) {
-
-            vm.contents=[];
-            vm.leads = response.data;
-
-            var openLeads=0;
-            var wonLeads=0;
-            var lostLeads=0;
-
-
-            for(var i = 0; i < vm.leads.length; i++) {
-
-                var obj ={
-                    descLead : vm.leads[i].DescLead,
-                    entidade : vm.leads[i].Entidade,
-                    estado : vm.leads[i].Estado,
-                    tipoEntidade : vm.leads[i].TipoEntidade,
-                    idlead : vm.leads[i].Idlead
-                };
-
-                if(obj.estado == 0)
-                {
-                    openLeads++;
-                }
-                else if(obj.estado == 1)
-                {
-                    wonLeads++;
-                }
-                else if(obj.estado == 2)
-                {
-                    lostLeads++;
-                }
-
-
-           }
-
-          
-
-           function chartPieData() {
-            vm.data = [];
-
-           vm.data.push(openLeads);
-           vm.data.push(wonLeads);
-           vm.data.push(lostLeads);
-        }
-
-        // init
-
-        chartPieData();     
-
-        // Simulate async data update
-        $interval(chartPieData, 5000);
-
-
-
-        vm.columns = [{
-            title: '',
-            field: 'thumb',
-            sortable: false,
-            filter: 'tableImage'
-        },{
-            title: 'Client',
-            field: 'entidade',
-            sortable: true
-        },{
-            title: 'Valor',
-            field: 'ValorTotalOV',
-            sortable: true
-        },{
-            title: 'Closing Date',
-            field: 'DataFecho',
-            sortable: true
-        }]; 
-
-
-          });
         
+
+
          
 
     });
 
-        
+      
     }
 })
 ();
